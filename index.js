0e39fabdafcadc22ee7842837d0a538c077e6b91
Amista <62178839+amistaa@users.noreply.github.com>
2020-05-26 00:12:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const Discord = require('discord.js');
+const rss = require('rss-converter');
 
+(async () => {
+    let feed = await rss.toJson('https://www.youtube.com/feeds/videos.xml?channel_id=' + channel_yt);
+    console.log(feed.items[0].media_group.media_description);
+})();
