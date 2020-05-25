beda3a11329d1b0025491026206c4c2af313ac0b
Amista <62178839+amistaa@users.noreply.github.com>
2020-05-26 01:22:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const rss = require('rss-converter');
 const config = require('./config.json');
-const links = require('./links.json');
 const client = new Discord.Client();
 
 client.on("ready", async () => {
     console.log("Login");
     setInterval(async () => {
     let feed = await rss.toJson('https://www.youtube.com/feeds/videos.xml?channel_id=' + config.channel_yt);
-    if (links.includes(feed.items[0].yt_videoId)) return;
     let jsonOpen = fs.readFileSync('links.json');
     let json = JSON.parse(jsonOpen);
+    if (links.includes(feed.items[0].yt_videoId)) return;
     json.push(feed.items[0].yt_videoId);
     let jsonLink = JSON.stringify(json);
     fs.writeFileSync('links.json', jsonLink);
